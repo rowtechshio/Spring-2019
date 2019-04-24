98932929c7044926be0a1c08518bc3bacafccaa5
rowtechshio <rowtechshio@gmail.com>
2019-04-24 15:31:53 -0700

    quickbackup

---
+++ direktive.py
@@ -0,0 +1,26 @@
+#!/brado/bin/python
+
+"""
+""CREATED BY: ROWTECHSHIO
+""DATE CREATED: 04/24/19
+""
+""ABOUT:
+""Direktive.py acts as a command line, all in one data manager
+""specifically built for the purpose of making a front-end GUI for easy file management
+""and data retrieval. 
+""
+""GOALS:
+""GUI should display all information about given show
+""GUI should have a search and launch feature
+""GUI needs to be flexible for file formats of any type
+""
+""
+"" ..w~~*         *~~W..     *~~W....W~~***~~W;;.,~*
+    .;;;**       **;;;.      .;;;***;;.**.**;;..**;~
+     ;;;***     ***;;;       ;;;**@         ;;;**as
+       */""',;,'""/*         */""'@         '""//D/
+        ""|d|||d|""          ""|d||aaDD||dddd||as**
+       iiuakl  lkauii        iiuaklualksii**{{}}
+      88babk    kbab88       88babk     babk8PPZZD
+    "YPWPY"      "YPWPY"     "YPWPY"     "WYPWY###Z
+   ##HZHH          HHZH##    ##HZHH        ##@HZHHWW
